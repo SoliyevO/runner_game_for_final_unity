cfa41d4a5c9652d044ad0b6639489833fcbe0ead
Otabek <146961339+SoliyevO@users.noreply.github.com>
2024-12-13 02:42:21 +0500

    update tile manager

--- runner_game_for_unity_final/Assets/Scripts/CameraController.cs
+++ runner_game_for_unity_final/Assets/Scripts/CameraController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraComtroller : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
 
     public Transform target;
