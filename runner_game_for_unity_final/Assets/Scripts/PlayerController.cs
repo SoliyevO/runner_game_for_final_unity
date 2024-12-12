2f5d65fcaef9c3e23249137e005654df48042c31
Otabek <146961339+SoliyevO@users.noreply.github.com>
2024-12-13 00:30:36 +0500

    add player controller script

--- runner_game_for_unity_final/Assets/Scripts/PlayerController.cs
+++ runner_game_for_unity_final/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private CharacterController controller;
+    public Vector3 direction;
+    public float forwardSpeed;
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        direction.z = forwardSpeed;
+    }
+    private void FixedUpdate()
+    {
+        controller.Move(direction * Time.fixedDeltaTime);
+
     }
 }
