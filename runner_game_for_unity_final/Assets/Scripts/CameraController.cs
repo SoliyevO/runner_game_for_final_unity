277932c3de67154e27a1a6008b2e7184a8765b7d
Otabek <146961339+SoliyevO@users.noreply.github.com>
2024-12-13 01:06:21 +0500

    camera controller script update

--- runner_game_for_unity_final/Assets/Scripts/CameraController.cs
+++ runner_game_for_unity_final/Assets/Scripts/CameraController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class CameraComtroller : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public Transform target;
+    private Vector3 offset;
+
     void Start()
     {
-        
+        offset = transform.position - target.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        Vector3 newPosition  = new Vector3(transform.position.x, transform.position.y, offset.z+target.position.z);
+        transform.position = Vector3.Lerp(transform.position, newPosition, 10* Time.deltaTime);
     }
 }
